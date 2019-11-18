8250ebc535174cad8e0bf4130812fac6bb0b05c2
Sana Zaidi <sana.rizvi.5@gmail.com>
2019-11-18 22:53:46 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,7 @@
-# Write your code here.
+name = ["Edsger", "Ada", "Charles", "Aln", "Grace", "Linus", "Matz"]
+
+def badge_maker(name)
+  name.each. do |name| 
+  puts "Hello, my name is #{name}."
+  end 
+end
